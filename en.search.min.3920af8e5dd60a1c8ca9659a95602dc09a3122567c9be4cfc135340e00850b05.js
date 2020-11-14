35eeb421c23d8b23867a30c578a46cd931a5993e
ppyv <ppyv@users.noreply.github.com>
2020-11-14 03:34:32 +0000

    deploy: 2af30473d8d5fc84f5386538db7bcd37ad97f245

--- en.search.min.3920af8e5dd60a1c8ca9659a95602dc09a3122567c9be4cfc135340e00850b05.js
+++ en.search.min.3920af8e5dd60a1c8ca9659a95602dc09a3122567c9be4cfc135340e00850b05.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/remote-work-handbook/flexsearch.min.js');loadScript('/remote-work-handbook/en.search-data.min.4f20f4c5726cf481ea8c3395d8866dada728670521201cff9b11243e70f7d6ba.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/remote-work-handbook/flexsearch.min.js');loadScript('/remote-work-handbook/en.search-data.min.179f5c5bbea444f539497a3d6965cb271181e589341b448bf6b22d26154fee3e.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
